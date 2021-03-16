fe9a661e1102ec295135355c1d108253af18fcfe
Dominik Lindner <d.lindner@dundee.ac.uk>
2021-03-16 15:19:40 +0000

    Fix image names

--- scripts/process_filepaths.py
+++ scripts/process_filepaths.py
@@ -7,7 +7,7 @@ filelist = "idr0107_files.txt"
 filepaths = "../experimentA/idr0107-experimentA-filePaths.tsv"
 
 #hei10 +-/hei10+-_2.12.19_plant13_slide1_image1/Path1.tif
-FORMAT = "(.+)/.*_(plant\d+_slide\d+_image\d+)/(.*\.tif)"
+FORMAT = "(.+)/(.*_plant\d+_slide\d+_image\d+)/(.*\.tif)"
 
 fpaths_out = open(filepaths, "w")
 entries = open(filelist, 'r').readlines()
@@ -16,12 +16,18 @@ for entry in entries:
     entry = entry.strip()
     if match := re.search(FORMAT, entry, re.IGNORECASE):
         dataset = match.group(1)
+        if dataset == "WT":
+            dataset = "Wild Type"
+        elif dataset == "hei10 +-":
+            dataset = "HEI10 +/- Heterozygous Mutant"
+        elif dataset == "HEI10 OX":
+            dataset = "HEI10 Over-expressor (C2)"
         image = match.group(2)
         filename = match.group(3)
         if filename.startswith("Composite"):
             fpaths_out.write(f"Dataset:name:{dataset}\t{basedir}/{entry}\t{image}\n")
         elif filename.startswith("Path"):
-            fpaths_out.write(f"Dataset:name:{dataset} masks\t{basedir}/{entry}\t{image}_{filename}\n")
+            fpaths_out.write(f"Dataset:name:{dataset} (segmentation)\t{basedir}/{entry}\t{image}_{filename}\n")
         else:
             print(f"Unusual entry: {entry}")
             fpaths_out.write(f"Dataset:name:{dataset}\t{basedir}/{entry}\t{filename}\n")
