b4259026ac27627c0b4528aa022beb31a377c22b
Dominik Lindner <d.lindner@dundee.ac.uk>
2021-06-23 13:25:51 +0100

    Fix import rendering script

--- scripts/import_renderingsettings.sh
+++ scripts/import_renderingsettings.sh
@@ -18,5 +18,5 @@ do
 	img=`echo ${file} | cut -d ',' -f 2 | sed "s/.yml//" | sed 's/|/\//'`
 	image_id=`omero hql -q --ids-only --limit 10 --style  plain "select img from DatasetImageLink l join l.parent as ds join l.child as img where ds.name = '$ds' and img.name = '$img'" | cut -d ',' -f 2 | sed 's/ImageI/Image/'`
 	echo "$file -> $ds / $img -> $image_id"
-	omero render set $image_id $file
+	omero render set $image_id "$file"
 done
